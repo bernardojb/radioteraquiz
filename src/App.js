d9904c544b03e6cefd2d128a002d831fa0213fbd
Bernardo Braga <bernardojbraga@gmail.com>
2021-12-02 15:23:34 -0300

    landing

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
